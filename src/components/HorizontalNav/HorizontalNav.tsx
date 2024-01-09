4204124e0103781640edc5a10c4328ea3680e72d
David Leal <david.leal@arpher.cl>
2024-01-09 15:27:50 -0300

    incorporacion desplazamiento horizontal

--- src/components/HorizontalNav/HorizontalNav.tsx
+++ src/components/HorizontalNav/HorizontalNav.tsx
@@ -1,31 +1,17 @@
 // Libraryes
 import { Link } from "react-scroll";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./HorizontalNav.scss";
-
-// Array Categorias
-const categorias = [
-  "Entradas",
-  "Carnes",
-  "Pescados",
-  "Postres",
-  "Aperitivos y Cocteles",
-  "Bebidas y cervezas",
-];
+import secciones from "../../json/Json";
 
 function HorizontalNav() {
-  const [selectedIndex, setSelectedIndex] = useState<number | null>(null);
   const [isFixed, setIsFixed] = useState(false);
-
-  const changeColor = (index: number) => {
-    setSelectedIndex(index);
-  };
+  const [firstClick, setFirstClick] = useState(true);
+  const observerRef = useRef<null | MutationObserver>(null);
 
   useEffect(() => {
     const handleScroll = () => {
       const offset = window.scrollY;
-
-      // Puedes ajustar este valor según sea necesario
       const triggerOffset = 100;
 
       setIsFixed(offset > triggerOffset);
@@ -34,40 +20,89 @@ function HorizontalNav() {
     window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener("scroll", handleScroll);
+      window.scrollTo(0, window.scrollY - 100);
     };
   }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  const cambiofirstClick = () => {
+    if (firstClick) {
+      setFirstClick(false);
+    }
+  };
+
+  useEffect(() => {
+    const targetNode = document.getElementById("navUl");
+
+    if (!targetNode) {
+      return;
+    }
+
+    const observer = new MutationObserver((mutations) => {
+      mutations.forEach((mutation) => {
+        const addedNodes = Array.from(mutation.addedNodes) as HTMLElement[];
+        addedNodes.forEach((node) => {
+          if (node.classList.contains("active")) {
+            despHorizontal(node.id);
+          }
+        });
+      });
+    });
+
+    const config = { childList: true, subtree: true };
+    observer.observe(targetNode, config);
+    observerRef.current = observer;
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
+  }, []); // Ejecutar el efecto solo una vez al montar el componente
+
+  const despHorizontal = (key: string) => {
+    if (key != null) {
+      const divUl = document.getElementById("navUl");
+      const targetElement = document.getElementById(key);
+
+      if (divUl != null && targetElement != null) {
+        const containerWidth = divUl.clientWidth;
+        const targetWidth = targetElement.clientWidth;
+        const scrollLeftValue =
+          targetElement.offsetLeft - (containerWidth - targetWidth) / 2;
+        divUl.scrollTo({
+          left: scrollLeftValue,
+          behavior: "smooth",
+        });
+      }
+    }
+  };
 
   return (
     <div className={`contHorNav ${isFixed ? "fixed" : ""}`}>
       <div>
-        <ul className="navUl">
-          {
-            // .map Categorias
-            categorias.map((categoria, index) => {
-              return (
-                <Link
-                  to={`categoria${index}`}
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                  offset={-108}
-                  className="navLi"
-                  key={categoria}
-                  onSetActive={() => changeColor(index)}
-                >
-                  <button
-                    className={`navLink ${
-                      index === selectedIndex ? "active" : ""
-                    }`}
-                    onClick={() => changeColor(index)}
-                  >
-                    {categoria}
-                  </button>
-                </Link>
-              );
-            })
-          }
+        <ul className="navUl" id="navUl">
+          {secciones.map((seccion, index) => (
+            <li className="nav-item" id={seccion.key} key={seccion.id}>
+              <Link
+                to={`categoria${index}`}
+                spy={true}
+                smooth={true}
+                duration={400}
+                offset={firstClick ? -162 : index === 0 ? -105 : -106}
+                className="nav-link"
+                onClick={() => {
+                  cambiofirstClick();
+                  despHorizontal(seccion.key);
+                }}
+              >
+                {seccion.nombre}
+              </Link>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
@@ -75,8 +110,3 @@ function HorizontalNav() {
 }
 
 export default HorizontalNav;
-
-// className={`navLink ${
-//   index === selectedIndex ? "actived" : ""
-// }`}
-// onClick={() => changeColor(index)}
---
+++ src/json/Json.tsx
@@ -0,0 +1,11 @@
+// Secciones del Restaurante
+const secciones = [
+  { id: 1, nombre: "Entradas", key: "101" },
+  { id: 2, nombre: "Carnes", key: "102" },
+  { id: 3, nombre: "Pescados", key: "103" },
+  { id: 4, nombre: "Postres", key: "104" },
+  { id: 5, nombre: "Aperitivos y Cocteles", key: "105" },
+  { id: 6, nombre: "Bebidas y Cervezas", key: "106" },
+];
+
+export default secciones;
